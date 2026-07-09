6b4d85e20e4aa3d8f10d5d09fe72870eec080bed
larrykennedy5-p <larrykennedyoh503@gmail.com>
2026-07-09 18:32:02 +0800

    fix: use own data repository

--- js/data-config.js
+++ js/data-config.js
@@ -12,13 +12,13 @@ const DATA_CONFIG = {
      * GitHub repository owner (username)
      * This will be replaced during GitHub Actions workflow execution
      */
-    repoOwner: 'dw-dengwei',
+    repoOwner: 'larrykennedy5-p',
 
     /**
      * GitHub repository name
      * This will be replaced during GitHub Actions workflow execution
      */
-    repoName: 'daily-arXiv-ai-enhanced',
+    repoName: 'paper_recom',
 
     /**
      * Data branch name
